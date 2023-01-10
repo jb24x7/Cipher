ef159156d9b33616eba720a850d567ce1f223788
James Provance <jb24x7@gmail.com>
2023-01-09 16:08:56 -0800

    takes first and last letters and capitalize

--- js/scrpts.js
+++ js/scrpts.js
@@ -1,18 +1,15 @@
-let UserInput = prompt("Enter a sentence:");
-// const firstLetter = UserInput.charAt(0);
-// const lastLetter = UserInput.at(-1);
+let userInput = prompt("Enter a sentence:");
 
+function firstandlastletter(first, last) {
+first = userInput.charAt(0);
+last = userInput.charAt(userInput.length-1);
 
-function firstandlastletter(UserInput) {
-let first = UserInput.charAt(0);
-let last = UserInput.charAt(UserInput.lenght-1);
-console.log(last);
 newSent = first.toUpperCase() + last.toUpperCase();
-console.log(newSent);
 
+return newSent;
 }
 
-firstandlastletter(UserInput);
+alert(firstandlastletter(userInput));
 
 
 
