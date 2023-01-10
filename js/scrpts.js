a047929cdf73fae4c9d2a06ae893cc53d844fa06
James Provance <jb24x7@gmail.com>
2023-01-09 16:29:53 -0800

    function to reverse user input

--- js/scrpts.js
+++ js/scrpts.js
@@ -1,6 +1,8 @@
 let userInput = prompt("Enter a sentence:");
+let testString = "";
 
 function firstandlastletter(first, last) {
+
 first = userInput.charAt(0);
 last = userInput.charAt(userInput.length-1);
 
@@ -9,7 +11,25 @@ newSent = first.toUpperCase() + last.toUpperCase();
 return newSent;
 }
 
-alert(firstandlastletter(userInput));
+  function reverseString(str) {
+    // Step 1. Use the split() method to return a new array
+    let splitString = str.split(""); // var splitString = "hello".split("");
+    // ["h", "e", "l", "l", "o"]
+ 
+    // Step 2. Use the reverse() method to reverse the new created array
+    let reverseArray = splitString.reverse(); // var reverseArray = ["h", "e", "l", "l", "o"].reverse();
+    // ["o", "l", "l", "e", "h"]
+ 
+    // Step 3. Use the join() method to join all elements of the array into a string
+    let joinArray = reverseArray.join(""); // var joinArray = ["o", "l", "l", 
+    
+    //Step 4. Return the reversed string
+    return joinArray; // "olleh"
+}
+
+testString = firstandlastletter(userInput);
+alert(reverseString(testString));
+
 
 
 
